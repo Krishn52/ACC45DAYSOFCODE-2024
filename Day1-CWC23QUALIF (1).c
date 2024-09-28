7977ae24749182f0837024ac55c3c8cf969a06a8
Krishn52 <krishnagupta52417@gmail.com>
2024-09-28 15:47:04 +0530

    Add files via upload

---
+++ Day1-CWC23QUALIF (1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("Your team points are ");
+    scanf("%d",&x);
+	if (x>=12){
+	printf("qualifed");
+	}
+	else{
+	printf("not qualified");
+	}
+	return 0;
+}
