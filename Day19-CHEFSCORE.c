44a62d470da0efddfa504ae74377e210208ae125
Krishn52 <krishnagupta52417@gmail.com>
2024-10-21 21:12:18 +0530

    Add files via upload

---
+++ Day19-CHEFSCORE.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    
+    while (T--) {
+        int N, X, Y;
+        scanf("%d %d %d", &N, &X, &Y);
+        
+        int max_score = N * X;
+        if (Y <= max_score && Y % X == 0 && (Y / X) <= N) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
